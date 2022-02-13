810d65786d22ee20d5333ccd08e58d4c864f3000
Jorge Pinell <jpinell1075@gmail.com>
2022-02-12 21:37:03 -0600

    Finish Date Class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ConceptsPOO;
+
+namespace ConceptsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POO Concepts");
+            Console.WriteLine("===========s");
+
+            try //Ctrl + ks
+            {
+                //Date date1 = new Date(2022, 02, 12);
+                //Console.WriteLine(date1);
+                Console.WriteLine(new Date(2022, 02, 28));
+                Console.WriteLine(new Date(2024, 02, 29));
+                Console.WriteLine(new Date(2023, 11, 29));
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
