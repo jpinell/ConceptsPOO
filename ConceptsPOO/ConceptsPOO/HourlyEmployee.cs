a048f42600082a0f6237deab8c0d02bfeaabf2a0
Jorge Pinell <jpinell1075@gmail.com>
2022-02-20 15:45:55 -0600

    Finish POO Concepts

---
+++ ConceptsPOO/ConceptsPOO/HourlyEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace ConceptsPOO
+{
+    public class HourlyEmployee : Employee
+    {
+        public float Hours { get; set; }
+        public decimal HourValue { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            return (decimal)Hours * HourValue;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{base.ToString()}\n" +
+                $"Horas............: {$"{Hours:N2}",15}\n" +
+                $"Valor por hora...: {$"{HourValue:C2}",15}\n" +
+                $"Salario..........: {$"{GetValueToPay():C2}",15}";
+        }
+    }
+}
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using ConceptsPOO;
 
 namespace ConceptsPOO
@@ -18,8 +20,9 @@ namespace ConceptsPOO
                 IsActive = true,
                 Salary = 10256.25M
             };
-            Console.WriteLine(employee1);
+            //Console.WriteLine(employee1);
             Console.WriteLine("=====================================================\n");
+            
             Employee employee2 = new CommissionEmployee()
             {
                 Id = 2020,
@@ -31,7 +34,72 @@ namespace ConceptsPOO
                 CommissionPercentaje = 0.25F,
                 Sales = 50000M
             };
-            Console.WriteLine(employee2);
+            //Console.WriteLine(employee2);
+            Console.WriteLine("=====================================================\n");
+            
+            Employee employee3 = new HourlyEmployee()
+            {
+                Id = 3030,
+                FirstName = "Amaru",
+                LastName = "Pinell",
+                BirthDate = new Date(2008, 02, 26),
+                HiringDate = new Date(2021, 09, 13),
+                IsActive = true,
+                Hours = 123.50F,
+                HourValue = 12356.56M
+            };
+            //Console.WriteLine(employee3);
+            Console.WriteLine("=====================================================\n");
+            
+            Employee employee4 = new BaseCommissionEmployee()
+            {
+                Id = 4040,
+                FirstName = "Amaru",
+                LastName = "Pinell",
+                BirthDate = new Date(2008, 02, 26),
+                HiringDate = new Date(2021, 09, 13),
+                IsActive = true,
+                Base = 860678.45M,
+                CommissionPercentaje = 0.015F,
+                Sales = 58000000M
+            };
+            //Console.WriteLine(employee4);
+
+            ICollection<Employee> employees = new List<Employee>()
+            {
+                employee1, employee2, employee3, employee4 //Agregar nuevos empleados
+            };
+
+            decimal payroll = 0;
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee);
+                payroll += employee.GetValueToPay();
+            }
+
+            Console.WriteLine("                       ===============");
+            Console.WriteLine($"TOTAL                  {$"{payroll:C2}",15}");
+
+            Invoice invoice1 = new Invoice()
+            {
+                Description = "iPhone 13",
+                Id = 1,
+                Price = 5300000M,
+                Quantity = 6
+            };
+
+
+            Invoice invoice2 = new Invoice()
+            {
+                Description = "Posta Premium",
+                Id = 2,
+                Price = 32000M,
+                Quantity = 17.5F
+            };
+
+            Console.WriteLine(invoice1);
+            Console.WriteLine(invoice2);
         }
 
         private static void LlamaClaseDate()
